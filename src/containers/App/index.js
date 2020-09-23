6a1e6556c6e697f877f30c09d847ed558dfe0e25
Julius Dejon <juliusdejon@gmail.com>
2020-09-23 17:51:08 +0800

    Added `D-DIN` as my font

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <div className="App">
+      <div className="navbar">Hello</div>
+      <div className="header">Header</div>
+      <div className="footer">Footer</div>
+    </div>
+  );
 }
 
 export default App;
