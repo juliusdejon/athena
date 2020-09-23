f3d25ba7fad67d40ee11d47f2690d12d020c25a5
Julius Dejon <juliusdejon@SP-JDejon-MacBookPro2019.local>
2020-09-23 17:02:06 +0800

    Moved things in a containers and components, Added react-icons, @reach/router for routing

---
+++ src/containers/App/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return <Router>hello</Router>;
+}
+
+export default App;
