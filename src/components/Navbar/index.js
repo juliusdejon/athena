4b3ad3a87bb69d708c7ca09f4230d93718d22ca1
Julius Dejon <juliusdejon@gmail.com>
2020-09-24 00:13:54 +0800

    Added Moon as our logo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 
+import logo from "assets/images/logo.png";
 import "./Navbar.css";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="navbar-container">
-        <div className="navbar-logo-container">JD</div>
+        <div className="navbar-logo-container">
+          <img src={logo} alt="space" />
+        </div>
         <div className="menu-list-container">
           <ul className="menu-list">
             <li className="menu">About</li>
