169e3f101d8ef0773fcd71fcef25aab0b2018d37
Julius Dejon <juliusdejon@gmail.com>
2020-09-24 22:29:29 +0800

    Added date-fns, compute exp by giving the year date

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Stars from "components/Stars";
+
+import differenceInYears from "date-fns/differenceInYears";
+
 import moon from "assets/images/moon.png";
 import shades from "assets/images/shades.png";
 
 import "./About.css";
 
 function About() {
+  const years_of_experience = differenceInYears(
+    new Date(),
+    new Date(2018, 8, 8)
+  );
   return (
     <>
       <div className="absolute-container">
@@ -23,15 +30,15 @@ function About() {
         <div className="long-description">
           <p>
             I'm a software engineer based in the Philippines specializing in web
-            platform. I have 2 years of experience in Developing Web 🕸️
-            applications. I'm inclined to cutting edge technologies, astronomy,
-            and financial stuffs.
+            platform. I have {years_of_experience} years of experience in
+            Developing Web 🕸️ applications. I'm inclined to cutting edge
+            technologies, astronomy, and financial stuffs.
           </p>
           <br />
           <p>
-            I have diverse skillset on React and Elixir. I'm also a certified
-            AWS Solutions Architect-associate. I love breaking things so I can
-            recreate them to understand the inspiration behind it.
+            I have diverse skillset on React and Elixir. I also have a
+            background in server linux administration and a certified AWS
+            Solutions Architect-Associate.
           </p>
         </div>
         <div className="earth-container">
