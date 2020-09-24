284f917e59cc0b12b5d085c990f36c93236fde99
Julius Dejon <juliusdejon@SP-JDejon-MacBookPro2019.local>
2020-09-24 23:19:35 +0800

    Updates

---
+++ precache-manifest.f4962c2fa497d1a579d3645e88e336c9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18e561404dff86bbad839b3c6e834a5",
+    "url": "/athena/index.html"
+  },
+  {
+    "revision": "0f633f29f7418bd0ee23",
+    "url": "/athena/static/css/main.3f8c35e0.chunk.css"
+  },
+  {
+    "revision": "0385ceaab8effbcf9cf4",
+    "url": "/athena/static/js/2.efde223b.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/athena/static/js/2.efde223b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f633f29f7418bd0ee23",
+    "url": "/athena/static/js/main.48513412.chunk.js"
+  },
+  {
+    "revision": "b87e67477766a20360bc",
+    "url": "/athena/static/js/runtime-main.e6493b04.js"
+  },
+  {
+    "revision": "0eaa1db69170023b522a2cba8895f226",
+    "url": "/athena/static/media/D-DIN-Bold.0eaa1db6.otf"
+  },
+  {
+    "revision": "386c9445edc3c8be8986181374b5fe73",
+    "url": "/athena/static/media/D-DIN.386c9445.otf"
+  },
+  {
+    "revision": "e62b18ad70e14311bc693ad93a6b7bb6",
+    "url": "/athena/static/media/buildings.e62b18ad.png"
+  },
+  {
+    "revision": "db7a11aca5441b9b7c732a927ec06596",
+    "url": "/athena/static/media/cursor.db7a11ac.cur"
+  },
+  {
+    "revision": "b141b126f59438e90ad66909977f9474",
+    "url": "/athena/static/media/earth.b141b126.png"
+  },
+  {
+    "revision": "fdec570ab06757a759e4199c37e1fa5c",
+    "url": "/athena/static/media/moon.fdec570a.png"
+  }
+]);
